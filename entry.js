947998d9a4d63c1e69d926196c8d2f2204d95a09
mygoare <mygoare@gmail.com>
2016-03-17 18:37:17 +0800

    import font-awesome for testing purpose (url-loader or file-loader deals with the font files)

--- entry.js
+++ entry.js
@@ -1,3 +1,8 @@
 import 'normalize.css';
+
+import 'font-awesome/css/font-awesome.css'
+
 require('./style.css');
 require('./react.js');
+
+//require('./redux')
--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,28 @@ module.exports = {
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel',
                 query: {
-                    presets: ['react', 'es2015']
                 }
+            },
+
+            {
+              test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+              loader: "url?limit=10000&mimetype=application/font-woff"
+            }, 
+            {
+              test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+              loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+              test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+              loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+              test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+              loader: "file"
+            },
+            {
+              test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+              loader: "url?limit=10000&mimetype=image/svg+xml"
             }
         ]
     },
